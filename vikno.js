f6a19649ead67b55a39d778e26e9fd3a18c33044
zamoklubarta <45072317+zamoklubarta@users.noreply.github.com>
2018-11-23 11:04:21 +0200

    Add files via upload

---
+++ vikno.js
@@ -0,0 +1,6 @@
+function vikno(){
+var popup = window.open('автори.html', 'Microsoft',
+'width = "500px",left="50%" "top=50%" height = 400px", resizable = yes');
+popup.moveTo(50,50);
+popup.resizeTo (800,580);
+}
